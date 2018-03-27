e5dc0ca6f18022c73fa6088233421f92df95c6ce
lovelytong <tongyu@tongyudeMacBook-Air.local>
2018-03-27 20:00:35 +0800

    武装飞船

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,8 +2,10 @@
 import sys
 
 import pygame
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
   #初始化游戏并创建一个屏幕对象
@@ -13,22 +15,16 @@ def run_game():
   pygame.display.set_caption("Alien Invasion")
 
   #创建一艘飞船
-  ship = Ship(screen)
+  ship = Ship(ai_settings, screen)
+
+  #创建一个用于存储子弹的编组
+  bullets = Group()
 
   #开始游戏的主循环
   while  True:
-
-    #监视键盘和鼠标事件
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        sys.exit()
-
-    #每次循环时都重绘屏幕
-    screen.fill(ai_settings.bg_color)
-    ship.blitme()
-
-    #让最近绘制的屏幕可见
-    pygame.display.flip()
-
+    gf.check_events(ai_settings, screen, ship, bullets)
+    ship.update()
+    gf.update_bullets(bullets)
+    gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
